626506bd2ad4836e0b90abb79abc4eb8c123d245
B2Hacker <brandon.cortes.1690@gmail.com>
2021-08-07 19:56:36 -0500

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": "http://localhost:3000",
+    "BASE_URL": "https://nextjs-ecommercelo.herokuapp.com/",
     "MONGODB_URL": "mongodb+srv://b2:Torchwood@cluster0.yjy0k.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET": "+`.RX%AFgA+?3encMj9uUB=bwWw8X%^!]C4-M]_:B=[W&wq*3D",
     "REFRESH_TOKEN_SECRET": "VV]n)8#@Ar9LXCDK3HA>Y*nR/T]tAs:d@Q9</}=ZzGZd^dPAmC=b,eVTuZudR-'`7f*b?4.>%F}C+&Db",
